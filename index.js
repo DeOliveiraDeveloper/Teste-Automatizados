a419235841ef3a97c6ecc01e943441c1544e6bc6
DeOliveiraDeveloper <monteirocaio4@gmail.com>
2024-06-11 14:15:11 -0300

    Primeiro commit

---
+++ index.js
@@ -0,0 +1,4 @@
+import {soma, nome} from './soma.js'
+
+console.log(soma(1,1));
+console.log(nome);
---
+++ soma.test.js
@@ -0,0 +1,5 @@
+const soma = require('./soma')
+
+test('Tem que verificar a parada ai', () => {
+    expect(soma(3,8)).toBe(11)
+})
